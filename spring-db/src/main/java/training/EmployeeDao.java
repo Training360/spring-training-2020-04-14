7ceefae344e63b7f31ab95047ae0018aebac07ef
vicziani <viczian.istvan@gmail.com>
2020-04-16 09:41:49 +0200

    JdbcTemplate

--- spring-db/src/main/java/training/EmployeeDao.java
+++ spring-db/src/main/java/training/EmployeeDao.java
@@ -1,64 +1,68 @@
 package training;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.sql.*;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 @Repository
 public class EmployeeDao {
 
-    private List<Employee> employees = Collections.synchronizedList(new ArrayList<>());
-
-    private AtomicLong sequence = new AtomicLong();
-
-    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
 
     public EmployeeDao(DataSource dataSource) {
-        this.dataSource = dataSource;
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    private static Employee toEmployee(ResultSet resultSet, int i) throws SQLException {
+        var id = resultSet.getLong("id");
+        var name = resultSet.getString("emp_name");
+        return new Employee(id, name);
     }
 
     public Employee saveEmployee(String name) {
-        var employee = new Employee(sequence.getAndIncrement(), name);
-        employees.add(employee);
-
-        try (var conn = dataSource.getConnection();
-        var ps = conn.prepareStatement("insert into employees(emp_name) values (?)")) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps =
+                    connection.prepareStatement("insert into employees(emp_name) values (?)",
+                            Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, name);
-            ps.executeUpdate();
-        }
-        catch (SQLException e) {
-            throw new IllegalStateException("Cannot insert", e);
-        }
+            return ps;
+        }, keyHolder
+        );
+        var id = keyHolder.getKey().longValue();
 
-        return employee;
+        return new Employee(id, name);
     }
 
     public List<Employee> getEmployees() {
-        return new ArrayList<>(employees);
+        return jdbcTemplate.query("select id, emp_name from employees",
+                EmployeeDao::toEmployee);
     }
 
     public Employee findEmployeeById(long id) {
-        return employees.stream().filter(e -> e.getId()== id).findFirst().orElseThrow(() -> new IllegalArgumentException("Not found"));
+        return jdbcTemplate.queryForObject("select id, emp_name from employees where id = ?",
+                new Object[]{id}, EmployeeDao::toEmployee);
     }
 
     public Employee updateEmployee(long id, String name) {
-        var employee = findEmployeeById(id);
-        employee.setName(name);
-        return employee;
+        jdbcTemplate.update("update employees set emp_name = ? where id = ?",
+                name, id);
+        return new Employee(id, name);
     }
 
     public void deleteEmployee(long id) {
-        employees = employees.stream().filter(e -> e.getId() != id).collect(Collectors.toList());
+        jdbcTemplate.update("delete from employees where id = ?", id);
     }
 
     public void emptyEmployees() {
-        employees.clear();
+
+        jdbcTemplate.update("delete from employees");
     }
 }
